89da2fba77fefc7554b9a2f3a27d09e1b364cbd0
Jeremy Golden <58534260+cpltunnel@users.noreply.github.com>
2020-05-01 17:37:57 -0500

    Initial upload

---
+++ randColor.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+  var style = document.getElementById("style");
+  var randButton = document.getElementById("randButton");
+  randButton.onclick = function(){
+	  var hexNum = "0123456789ABCDEF";
+	  function randGen(){
+	    var randNum = Math.floor(Math.random() * 16);
+		return randNum;
+	  }
+	  var randHex = hexNum.charAt(randGen()) + hexNum.charAt(randGen()) + hexNum.charAt(randGen()) + hexNum.charAt(randGen()) + hexNum.charAt(randGen()) + hexNum.charAt(randGen());
+	  style.innerHTML = "body {background-color: #" + randHex + ";}"
+  };
+};
